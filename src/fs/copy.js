ccc52cf1ad0dc2cef2856b5ed05d8e72640ab26f
Volha Shved <v.shved@godeltech.com>
2022-12-04 20:51:42 +0300

    fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename)
+
+    const srcDir = path.join(__dirname, 'files');
+    const destDir = path.join(__dirname, 'files_copy');
+
+    try {
+        await fs.cp(srcDir, destDir, {
+            recursive: true,
+            errorOnExist: true,
+            force: false
+        })
+      } catch (err) {
+        if(err.code === 'ENOENT' || err.code === 'ERR_FS_CP_EEXIST') {
+            throw new Error('FS operation failed')
+        }
+      }
+
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const newPath = path.join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        await fs.access(newPath)
+        throw new Error('FS operation failed')
+    } catch (err) {
+        if(err.code === 'ENOENT') {
+            try {
+                await fs.rename(oldPath, newPath)
+            } catch (error) {
+                throw new Error('FS operation failed')
+            }
+        }
+        else {
+            throw err
+        }
+    }
 };
 
 await rename();
