40b1aadab2d60c8623dbd609e97761cfae9f6014
Volha Shved <v.shved@godeltech.com>
2022-12-05 22:48:41 +0300

    streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filepath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const readable = fs.createReadStream(filepath);
+    readable.on('data', (chunk) => {
+        process.stdout.write(chunk.toString());
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filepath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const writable = fs.createWriteStream(filepath);
+
+    process.stdin.on('data', chunk => {
+        writable.write(chunk.toString())
+    });
+
+    process.stdin.emit('data', 'abc');
 };
 
 await write();
